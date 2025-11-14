f12fb7c9e3f5d29c11c3f885378c5fc448f3775e
mannammurali <muralimannam@gmail.com>
2025-11-14 08:08:54 -0500

    Update TestJoin.sql

--- src/sql/TestJoin.sql
+++ src/sql/TestJoin.sql
@@ -1,40 +1,50 @@
-WITH MonthlySales AS ( 
-SELECT
-CAST(FORMAT(SaleDate, 'YYYY-MM-01') AS DATE) AS SaleMonth,
-Region,
-ProductID,
-SUM(SaleAmount) AS TotalSales
-FROM SalesData
-GROUP BY 1, 2, 3
+WITH MONTHLYSALES AS (
+    SELECT
+        CAST(FORMAT(SALEDATE, 'YYYY-MM-01') AS DATE) AS SALEMONTH,
+        REGION,
+        PRODUCTID,
+        SUM(SALEAMOUNT) AS TOTALSALES
+    FROM SALESDATA
+    GROUP BY 1, 2, 3
 ),
-RankedProducts AS (
-SELECT
-SaleMonth,
-Region,
-ProductID,
-TotalSales,
-RANK() OVER (PARTITION BY SaleMonth, Region ORDER BY TotalSales DESC) AS ProductRank
-FROM MonthlySales
-),
-  
-PreviousMonthSales AS (
-SELECT
-SaleMonth,
-Region,
-ProductID,
-TotalSales AS CurrentMonthSales,
-LAG(TotalSales, 1, 0) OVER (PARTITION BY Region, ProductID ORDER BY SaleMonth) AS PreviousMonthSales
-FROM MonthlySales
-)
-SELECT rp.SaleMonth,        rp.Region,                   rp.ProductID,
-rp.TotalSales,
-rp.ProductRank,
-(pms.CurrentMonthSales - pms.PreviousMonthSales) AS SalesDifferenceFromPreviousMonth
-FROM RankedProducts rp
-JOIN PreviousMonthSales pms
-ON rp.SaleMonth = pms.SaleMonth
-AND rp.Region = pms.Region
-AND rp.ProductID = pms.ProductID
-WHERE rp.ProductRank <= 5
-ORDER BY rp.SaleMonth DESC, rp.Region, rp.ProductRank;
 
+RANKEDPRODUCTS AS (
+    SELECT
+        SALEMONTH,
+        REGION,
+        PRODUCTID,
+        TOTALSALES,
+        RANK()
+            OVER (PARTITION BY SALEMONTH, REGION ORDER BY TOTALSALES DESC)
+            AS PRODUCTRANK
+    FROM MONTHLYSALES
+),
+
+PREVIOUSMONTHSALES AS (
+    SELECT
+        SALEMONTH,
+        REGION,
+        PRODUCTID,
+        TOTALSALES AS CURRENTMONTHSALES,
+        LAG(TOTALSALES, 1, 0)
+            OVER (PARTITION BY REGION, PRODUCTID ORDER BY SALEMONTH)
+            AS PREVIOUSMONTHSALES
+    FROM MONTHLYSALES
+)
+
+SELECT
+    RP.SALEMONTH,
+    RP.REGION,
+    RP.PRODUCTID,
+    RP.TOTALSALES,
+    RP.PRODUCTRANK,
+    (PMS.CURRENTMONTHSALES - PMS.PREVIOUSMONTHSALES)
+        AS SALESDIFFERENCEFROMPREVIOUSMONTH
+FROM RANKEDPRODUCTS AS RP
+INNER JOIN PREVIOUSMONTHSALES AS PMS
+    ON
+        RP.SALEMONTH = PMS.SALEMONTH
+        AND RP.REGION = PMS.REGION
+        AND RP.PRODUCTID = PMS.PRODUCTID
+WHERE RP.PRODUCTRANK <= 5
+ORDER BY RP.SALEMONTH DESC, RP.REGION ASC, RP.PRODUCTRANK ASC;
